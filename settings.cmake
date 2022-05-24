390c164482c730a082c10abe181164584787c543
Kevin Tran <z5164322@unsw.edu.au>
2022-05-24 19:45:00 +1000

    obviate cmake rerun with file copy target

--- settings.cmake
+++ settings.cmake
@@ -1,3 +1,4 @@
-set(SEL4CP_BUILD_DIR "${CMAKE_CURRENT_LIST_DIR}/tmp_build/timer" CACHE STRING "")
-set(CDL_TARGET "timer.cdl" CACHE STRING "")
+set(SEL4CP_BUILD_DIR "${CMAKE_CURRENT_LIST_DIR}/tmp_build/hello-goodbye" CACHE STRING "Directory containing cdl and elf files.")
+set(SEL4CP_CDL_FILENAME "hello.cdl" CACHE STRING "")
+set(SEL4CP_ELF_FILENAMES "hello.elf;goodbye.elf" CACHE STRING "")
 set(PLATFORM "zynqmp" CACHE STRING "")
