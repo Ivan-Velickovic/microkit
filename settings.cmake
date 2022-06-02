c2cc503f717325b00ab6362e5296aa95290afe95
Kevin Tran <z5164322@unsw.edu.au>
2022-06-02 19:38:12 +1000

    move zfloglevel to settings.cmake

--- settings.cmake
+++ settings.cmake
@@ -1,4 +1,6 @@
-set(SEL4CP_BUILD_DIR "${CMAKE_CURRENT_LIST_DIR}/tmp_build/hello-goodbye" CACHE STRING "Directory containing cdl and elf files.")
-set(SEL4CP_CDL_FILENAME "hello.cdl" CACHE STRING "")
-set(SEL4CP_ELF_FILENAMES "hello.elf;goodbye.elf" CACHE STRING "")
+set(SEL4CP_BUILD_DIR "${CMAKE_CURRENT_LIST_DIR}/tmp_build/timer" CACHE STRING "Directory containing cdl and elf files.")
+set(SEL4CP_CDL_FILENAME "timer.cdl" CACHE STRING "")
+set(SEL4CP_ELF_FILENAMES "timer.elf" CACHE STRING "")
 set(PLATFORM "zynqmp" CACHE STRING "")
+# zf_log_debug
+# set(LibUtilsDefaultZfLogLevel 2)
